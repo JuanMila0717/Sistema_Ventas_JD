8ed657105f8b4dd538e00815e205400d72fa4486
Juan Mila <emprendimientopower10@gmail.com>
2026-05-02 13:59:38 -0500

    feat: Creamos conexión a la base de datos exitosa y agregamos el .jar mysql para la conexión

---
+++ src/database/Conexion.java
@@ -0,0 +1,48 @@
+
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class Conexion {
+    
+    private final String DRIVER  = "com.mysql.cj.jdbc.Driver"; // Es un driver porque necesitamos componentes externos 
+    private final String URL = "jdbc:mysql://localhost:3306";
+    private final String DB = "dbsistema";
+    private final String USER = "root";
+    private final String PASSWORD = "";
+    
+    public Connection cadena;
+    
+    
+    public Conexion(){
+        this.cadena = null;
+    }
+    
+    public Connection conectar(){
+        try {
+           Class.forName(DRIVER); //Conexion a base de datos
+           this.cadena=DriverManager.getConnection(URL+DB,USER,PASSWORD); // Creando conexion con todos los datos
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        
+        return this.cadena;
+    }
+    
+    public void desconectar(){
+        try {
+            this.cadena.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+    
+    
+    
+    
+}
